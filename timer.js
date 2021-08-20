30ece8e09c1bfecfc8daeb3562e42eb256e5bd8c
NamHoaHong <namphamhagia@example.com>
2021-08-20 23:19:01 +0700

    update js lan 1

--- timer.js
+++ timer.js
@@ -1,3 +1,44 @@
-function setTimer(){
+let second = document.getElementById("second");
+let minute = document.getElementById("minute");
+let hour = document.getElementById("hour");
+let startBtn = document.getElementById("start");
+let resetBtn = document.getElementById("reset");
 
+
+let time = 15;
+
+if(time<10){
+    second.innerHTML = `0${time}`;
+    }
+    else{
+        second.innerHTML = `${time}`
+    }
+
+const countDown = setInterval(function(){
+    time--;
+    if(time<10){
+    second.innerHTML = `0${time}`;
+    }
+    else{
+        second.innerHTML = `${time}`
+    }
+    if(time<=0){
+        clearInterval(countDown)
+    }
+},1000);
+
+function start(){
+    if(startBtn.innerHTML == "start"){
+        startBtn.innerHTML = "stop";
+        countDown
+    }
+    else if(startBtn.innerHTML == "stop"){
+        startBtn.innerHTML = "start";
+        clearInterval(countDown);
+        let remaining = second.innerHTML
+    }
+};
+function reset(){
+    second.innerHTML = "00";
+    clearInterval(countDown)
 }
