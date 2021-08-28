75f65d45f6edf1e538755a90ac3f0a9e1fb4b462
NamHoaHong <namphamhagia@example.com>
2021-08-28 23:31:32 +0700

    done timer

--- timer.js
+++ timer.js
@@ -5,40 +5,78 @@ let startBtn = document.getElementById("start");
 let resetBtn = document.getElementById("reset");
 
 
-let time = 15;
+let time = 0;
 
-if(time<10){
-    second.innerHTML = `0${time}`;
-    }
-    else{
-        second.innerHTML = `${time}`
-    }
-
-const countDown = setInterval(function(){
-    time--;
-    if(time<10){
-    second.innerHTML = `0${time}`;
-    }
-    else{
-        second.innerHTML = `${time}`
-    }
-    if(time<=0){
-        clearInterval(countDown)
-    }
-},1000);
+var countDown;
+var x;
+var y;
+var z;
 
 function start(){
-    if(startBtn.innerHTML == "start"){
-        startBtn.innerHTML = "stop";
-        countDown
-    }
-    else if(startBtn.innerHTML == "stop"){
-        startBtn.innerHTML = "start";
-        clearInterval(countDown);
-        let remaining = second.innerHTML
-    }
+    countDown = setInterval(function(){
+        time--;
+        if(time<10){
+        second.innerHTML = `0${time}`;
+        }
+        else{
+            second.innerHTML = `${time}`
+        }
+        if(time == -1){
+            var m = minute.innerHTML;
+            m -= 1;
+            if(m<10){
+            minute.innerHTML = `0${m}`;
+            }
+            else{
+            minute.innerHTML = `${m}`
+            }
+            time = "59";
+            second.innerHTML = "59";
+            if(m == -1){
+                var h = hour.innerHTML;
+                h -= 1;
+                if(h<10){
+                hour.innerHTML = `0${h}`;
+                }
+                else{
+                hour.innerHTML = `${m}`
+                };
+                m = "59";
+                minute.innerHTML = "59";
+                if(h<0){
+                    clearInterval(countDown);
+                    hour.innerHTML = "00";
+                    minute.innerHTML = "00";
+                    second.innerHTML = "00";
+                }
+            }
+        }
+    },1000);
 };
-function reset(){
-    second.innerHTML = "00";
-    clearInterval(countDown)
+function stop(){
+    clearInterval(countDown);
+};
+function setTimer(){
+    var x = prompt("Input Hour (Between 0 and 23)");
+    if(x<10){
+    hour.innerHTML = `0${x}`;
+    }
+    else{
+    hour.innerHTML = `${x}`
+    };
+    var y = prompt("Input Minute (Between 0 and 60)");
+    if(y<10){
+    minute.innerHTML = `0${y}`;
+    }
+    else{
+    minute.innerHTML = `${y}`
+    };
+    var z = prompt("Input Second (Between 0 and 60)");
+    if(z<10){
+    second.innerHTML = `0${z}`;
+    }
+    else{
+    second.innerHTML = `${z}`
+    };
+    time = z;
 }
